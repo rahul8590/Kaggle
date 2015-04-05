69218bd3e047e5d69f134fbcab0fbbe694e03ee9
rahul <rahul8590@gmail.com>
2015-04-04 22:54:23 -0400

    Refactored to distributed code computation, logis seems fine

--- hw3/feature.py
+++ hw3/feature.py
@@ -20,6 +20,39 @@ def readfile():
 	return tfile
 
 
+def map_group(pgroup):
+	x = np.zeros(19)
+	x[0] = 1
+	value_list = pgroup[1]
+	for val in value_list:
+		fno = val[0].split('.')[0]
+		x[int(fno)-5] = val[1]
+	return x
+
+
+def upd_t23(tgbr):
+	key = tgbr[0]
+	hrs = tgbr[1]
+	if dtarget.has_key(key): #Checking if the hrs for 23rd exist
+		hrs[18] = dtarget[key]
+	return hrs
+
+
+
+def group(tfile):
+	tgbr = tfile.map(lambda d: ((d[0][0],d[0][2]),[(d[0][1],d[1])])) \
+				.reduceByKey(lambda p,q:p+q) \
+				.map(lambda d: (d[0], map_group(d)))
+	tgbr.first()
+	'''
+	should output the following 
+	((u'cn', u'suability'),
+		array([  1.,  26.,  38.,  16.,   0.,  33.,  41.,  35.,  13.,  24.,  39.,
+         9.,  17.,   0.,  56.,  30.,   0.,   0.,   0.]))
+	'''
+	return tgbr
+
+#The below function might soon be deprecated
 def create_feature(tfile):
 	collection = tfile.collect()
 	d = {}
@@ -59,9 +92,19 @@ def ols(mat):
 	#below equation solves the least square solution
 	doutput = np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
 	print "the dot product output is ",doutput
+	print doutput.shape
 
-	output = np.linalg.lstsq(x, y)[0]
-	print "the output for lstsq is ", output
+	#Solution 2
+	#output = np.linalg.lstsq(x, y)[0]
+	#print "the output for lstsq is ", output
+
+	'''
+	print "calc ydash, this is not working "
+	ydash = np.dot(np.squeeze(np.asarray(doutput.T)),x)
+	print ydash
+	'''
+	
+
 	''''
 	plt.plot(x, y, 'o', label='Original data', markersize=10)
 	plt.plot(x, m*x + c, 'r', label='Fitted line')
@@ -69,32 +112,38 @@ def ols(mat):
 	plt.show()
 	'''
 
+#Function to take in the arrays and then 
+# array([  1.,  26.,  38.,  16.,   0.,  33.,  41.,  35.,  13.,  24.,  39.,
+#        9.,  17.,   0.,  56.,  30.,   0.,   0.,  58.])
+#Calculate Sig (x,x^t)  and (xy)
+def calc(x):
+	pred = x[:18]
+	sig1 = np.outer(pred, pred)
+	sig2 = pred * x[18]
+	return sig1
+
+def calc_s2(x):
+	pred = x[:18]
+	sig2 = pred * x[18]
+	return sig2
+
+
 def main():
 	tfile = readfile()
-	dcollec = create_feature(tfile)
-	print "the aggregated data in 6-22 collections is",dcollec
+
 	dtarget = get_target_info()
 	print "the data in 23rd file is", dtarget
-	for key in dtarget:
-		if dcollec.has_key(key):
-			x = dcollec[key]
-			x[18] = dtarget[key]
-			dcollec[key] = x
-		else:
-			print "the key",key,"is not the dcollection... "
-			time.sleep(2)
-	print "the update aggregated data in dcollec is ",dcollec
-	mat = np.matrix(dcollec.values())
-	ols(mat)
 
+	tgbr = group(tfile)
+
+	tupdate = tgbr.map(lambda d: (d[0],upd_t23(d)))
+
+	#mat = np.matrix(tupdate.values().collect())
+	#ols(mat)
+	sig1 = tupdate.map(lambda d: (calc(d[1]))).reduce(lambda p,q:np.add(p,q))
+	sig2 = tupdate.map(lambda d: (calc_s2(d[1]))).reduce(lambda p,q:np.add(p,q))
 
 if __name__ == '__main__':
 	conf = SparkConf().setAppName('hw3').setMaster("master")
 	sc = SparkContext(conf=conf)
 	main()
-
-
-'''
-sc.textFile(base+'/'+str(fno)+'.txt')
-tfile_rdd = tfile.map(lambda view: set_feature(view))
-'''
